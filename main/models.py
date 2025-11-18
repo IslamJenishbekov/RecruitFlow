7a65c25c69399a7ed3ef0f37e4b35f5ce5d7c3f6
Islam Zhenishbekov <izhenishbekov@example.com>
2025-11-18 18:14:18 +0500

    update

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    # Добавьте свои поля здесь, например:
+    # age = models.PositiveIntegerField(null=True, blank=True)
+    pass
+
+    def __str__(self):
+        return self.username
